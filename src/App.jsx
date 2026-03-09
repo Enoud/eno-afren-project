2625acc34b50ea10a3d3e30e2ec2b83c946633fe
Enobong <enobongudoh455@gmail.com>
2026-03-09 15:20:05 +0100

    Fix base path for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './Pages/Landingpage'
 import Login from './Pages/Login'
 import Signup from './Pages/Signup'
